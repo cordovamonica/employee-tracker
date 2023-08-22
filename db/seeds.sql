e4e8b1d1a7868216e239a16c2c1967ce6f5b7ddd
Mona Lisa <cordovamonica.a@gmail.com>
2023-08-21 22:22:16 -0500

    added to seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO departments (name) 
+VALUES ('Sales'), ('Finance'), ('Human Resources');
+
+INSERT INTO role (department_id, title, salary)
+VALUES (1, 'Sales Lead', 40000), (2, 'Account Manager', 50000), (3, 'HR Manager', 60000);
+
+INSERT INTO employee (role_id, manager_id, first_name, last_name)
+VALUES (2, 1, 'Mike', 'Chan'), (3, 0, 'Ashley', 'Rodriguez'), (3, 0, 'Malia', 'Brown');
